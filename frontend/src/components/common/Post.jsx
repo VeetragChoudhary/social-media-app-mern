1cf6574a5b9b8c3a2cb6521f28e0da52d066bcbb
VeetragChoudhary <veetragchoudhary@gmail.com>
2024-07-31 11:12:10 +0530

    feat - like and comment functionality

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -8,13 +8,19 @@ import { Link } from "react-router-dom"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import toast from "react-hot-toast"
 import LoadingSpinner from "./LoadingSpinner"
+import { formatPostDate } from "../../utils/date"
 
 const Post = ({ post }) => {
   const [comment, setComment] = useState("")
   const { data: authUser } = useQuery({ queryKey: ["authUser"] })
   const queryClient = useQueryClient()
 
-  const { mutate: deletePost, isPending } = useMutation({
+  const postOwner = post.user
+  const isLiked = post.likes.includes(authUser._id)
+  const isMyPost = authUser._id === post.user._id
+  const formattedDate = formatPostDate(post?.createdAt)
+
+  const { mutate: deletePost, isPending: isDeleting } = useMutation({
     mutationFn: async () => {
       try {
         const res = await fetch(`/api/posts/${post._id}`, {
@@ -35,14 +41,60 @@ const Post = ({ post }) => {
     },
   })
 
-  const postOwner = post.user
-  const isLiked = false
+  const {mutate: likePost, isPending: isLiking} = useMutation({
+    mutationFn: async() => {
+      try {
+        const res = await fetch(`/api/posts/like/${post._id}`, {
+          method: "POST"
+        })
+        const data = await res.json()
+        if(!res.ok) throw new Error(data.error || "Something went wrong")
+        return data
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
+    onSuccess: (updatedLikes) => {
+      toast.success("Post liked successfully")
+      // queryClient.invalidateQueries({queryKey: ["posts"]}) // not the best UX. it will fetch all the posts
+      // instead, update the cache directly for that post
+      queryClient.setQueryData(["posts"], (oldData) => {
+        return oldData.map((p) => {
+          if(p._id === post._id) {
+            return {...p, likes: updatedLikes}
+          }
+          return p
+        })
+      })
+    },
+    onError: (error) => {
+      toast.error(error)
+    }
+  })
 
-  const isMyPost = authUser._id === post.user._id
-
-  const formattedDate = "1h"
-
-  const isCommenting = false
+  const {mutate: commentPost, isPending: isCommenting} = useMutation({
+    mutationFn: async (comment) => {
+      try {
+        const res = await fetch(`/api/posts/comment/${post._id}`, {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({text: comment})
+        })
+        const data = await res.json()
+        if(!res.ok) throw new Error(data.error || "Something went wrong")
+        return data
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
+    onSuccess: () => {
+      toast.success("Commented on post successfully")
+      queryClient.invalidateQueries({queryKey: ["posts"]})
+    },
+    onError: (error) => {
+      toast.error(error)
+    }
+  })
 
   const handleDeletePost = () => {
     deletePost()
@@ -50,9 +102,14 @@ const Post = ({ post }) => {
 
   const handlePostComment = (e) => {
     e.preventDefault()
+    if(isCommenting) return
+    commentPost(comment)
   }
 
-  const handleLikePost = () => {}
+  const handleLikePost = () => {
+    if(isLiking) return
+    likePost()
+  }
 
   return (
     <>
@@ -79,13 +136,13 @@ const Post = ({ post }) => {
             </span>
             {isMyPost && (
               <span className="flex justify-end flex-1">
-                {!isPending && (
+                {!isDeleting && (
                   <FaTrash
                     className="cursor-pointer hover:text-red-500"
                     onClick={handleDeletePost}
                   />
                 )}
-                {isPending && <LoadingSpinner size="sm" />}
+                {isDeleting && <LoadingSpinner size="sm" />}
               </span>
             )}
           </div>
@@ -186,16 +243,17 @@ const Post = ({ post }) => {
                 className="flex gap-1 items-center group cursor-pointer"
                 onClick={handleLikePost}
               >
-                {!isLiked && (
+                {isLiking && <LoadingSpinner size="sm"/>  }
+                {!isLiked && !isLiking && (
                   <FaRegHeart className="w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500" />
                 )}
-                {isLiked && (
+                {isLiked && !isLiking &&(
                   <FaRegHeart className="w-4 h-4 cursor-pointer text-pink-500 " />
                 )}
 
                 <span
-                  className={`text-sm text-slate-500 group-hover:text-pink-500 ${
-                    isLiked ? "text-pink-500" : ""
+                  className={`text-sm  group-hover:text-pink-500 ${
+                    isLiked ? "text-pink-500" : "text-slate-500"
                   }`}
                 >
                   {post.likes.length}
